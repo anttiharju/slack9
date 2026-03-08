18bd1d577424f0f5255a744f34aaad61dc2a860a
Antti Harju <antti@harju.io>
2026-03-08 10:26:31 +0200

    Use jiff instead of manually copying some alg.

--- src/slack/api_log.rs
+++ src/slack/api_log.rs
@@ -28,36 +28,16 @@ impl ApiLog {
 }
 
 fn format_utc(t: SystemTime) -> String {
-    let dur = t.duration_since(UNIX_EPOCH).unwrap();
-    let secs = dur.as_secs();
-    let millis = dur.subsec_millis();
-
-    let days = secs / 86400;
-    let time_secs = secs % 86400;
-    let hours = time_secs / 3600;
-    let minutes = (time_secs % 3600) / 60;
-    let seconds = time_secs % 60;
-
-    // Convert days since epoch to Y-M-D
-    let (year, month, day) = days_to_date(days);
-
+    let ts = jiff::Timestamp::try_from(t).unwrap();
+    let dt = ts.to_zoned(jiff::tz::TimeZone::UTC).datetime();
     format!(
         "{:04}-{:02}-{:02} {:02}:{:02}:{:02}.{:03}",
-        year, month, day, hours, minutes, seconds, millis
+        dt.year(),
+        dt.month(),
+        dt.day(),
+        dt.hour(),
+        dt.minute(),
+        dt.second(),
+        dt.subsec_nanosecond() / 1_000_000
     )
-}
-
-fn days_to_date(days_since_epoch: u64) -> (u64, u64, u64) {
-    // Civil calendar algorithm from http://howardhinnant.github.io/date_algorithms.html
-    let z = days_since_epoch + 719468;
-    let era = z / 146097;
-    let doe = z - era * 146097;
-    let yoe = (doe - doe / 1460 + doe / 36524 - doe / 146096) / 365;
-    let y = yoe + era * 400;
-    let doy = doe - (365 * yoe + yoe / 4 - yoe / 100);
-    let mp = (5 * doy + 2) / 153;
-    let d = doy - (153 * mp + 2) / 5 + 1;
-    let m = if mp < 10 { mp + 3 } else { mp - 9 };
-    let y = if m <= 2 { y + 1 } else { y };
-    (y, m, d)
 }
